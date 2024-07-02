7d4a2910ad6842c5c4129e4ec8fd2b7d615a75bc
perryjre <perryjre@github.com>
2024-07-02 19:45:24 -0400

    linting

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-#from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -13,7 +13,7 @@ class CarMake(models.Model):
 
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -22,10 +22,10 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+            validators=[
+                MaxValueValidator(2023),
+                MinValueValidator(2015)
+            ])
 
     def __str__(self):
         return "Name: " + self.name + "," + \
