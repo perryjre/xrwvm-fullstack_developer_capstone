d5239da2db6cbcb94fb25fd21feac6cfdb394b3c
perryjre <perryjre@github.com>
2024-07-02 21:42:06 -0400

    adds docker stuff

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -136,9 +136,9 @@ def get_dealer_details(request, dealer_id):
 
 def add_review(request):
     if (request.user.is_anonymous is False):
-        # data = json.loads(request.body)
+        data = json.loads(request.body)
         try:
-            # response = post_review(data)
+            post_review(data)
             return JsonResponse({"status": 200})
         except Exception:
             return JsonResponse({"status": 401,
